9e4d018877a23d2510edb2f02dd1eafcfdd7317b
nwwave <nwruswave@gmail.com>
2021-05-10 14:32:06 +0300

    Add scripts (lesson 2)

---
+++ frontend/pushImage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+repositoryName=registry.gitlab.com/club-map/main/frontend
+imageFullName=$repositoryName:latest
+
+echo [Main App STARTING] pushing image $imageFullName..
+
+echo [Main App] pushing image
+docker push $imageFullName
+
+echo [Main App FINISHED] image $imageFullName pushed
+
