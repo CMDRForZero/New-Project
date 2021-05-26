eca617bc198a7c310a4d0b88d6f9927508e09cfe
Vessels.TSS <an2288822@gmail.com>
2021-05-26 22:34:53 +0300

    - add yandex map to map.html

---
+++ frontend/source/js/event_reverse_geocode.js
@@ -0,0 +1,48 @@
+console.log('startscript');
+ymaps.ready(init);
+
+function init() {
+	console.log('startscript');
+	var myPlacemark, newMark,
+		myMap = new ymaps.Map('map', {
+			center: [55.753994, 37.622093],
+			zoom: 9
+		}, {
+			searchControlProvider: 'yandex#search'
+		});
+
+	// клик по карте
+	myMap.events.add('click', function (e) {
+		var coords = e.get('coords');
+		console.log(coords);
+
+		const name = prompt('Название метки')
+		const ballon = prompt('описание')
+		console.log(name, ballon)
+		myPlacemark = createPlacemark(coords, name, ballon);
+		//   newMark = createPlacemark([55.787281730573696, 38.23046091992188])
+		myMap.geoObjects.add(myPlacemark);
+
+	});
+
+	// Создание метки
+	function createPlacemark(coords, name, ballon) {
+		return new ymaps.Placemark(coords, {
+			//  balloonContentHeader:'<div class="place"><img src="place1.png" class="place1"></div>', name,
+			balloonContentHeader: ['<div class="place"><img src="./img/place1.png" class="place1" alt="метка">' + name + '</div>'].join(''),
+			iconCaption: name,
+			balloonContentFooter: 'Ваше событие!',
+			balloonContent: ballon
+
+		}, {
+			iconLayout: 'default#image',
+			iconImageHref: './source/place/bike.png',
+			iconImageSize: [35, 47],
+			iconImageOffset: [-19, -44]
+			// preset: 'islands#violetDotIconWithCaption',
+			// draggable: false
+		});
+	}
+
+
+}
