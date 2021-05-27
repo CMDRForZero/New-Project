10c87f5432051cadac71d5d06d894134e4e3fdeb
Alex Frolov <finddelst@gmail.com>
2021-05-27 18:13:55 +0300

    - yandex map add

--- frontend/src/components/map__yandex.js
+++ frontend/src/components/map__yandex.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import '../css/search.css';
-import '../js/event_reverse_geocode.js';
-
+import {Map, Placemark, YMaps} from "react-yandex-maps";
 
 const Mapyandex = () => {
-	return (
-		<div class="map__yandex" id="map">
-		</div>
-	);
+  const [placemarks, setPlacemarks] = useState(null);
+
+  return (
+    <div className="map__yandex" id="map">
+      <YMaps>
+        <Map
+          width="100%" height="100%"
+          defaultState={{center: [55.753994, 37.622093], zoom: 9}}
+          onClick={(e) => click(e)}
+				>
+					{placemarks && <Placemark
+						geometry={placemarks.coords}
+					>
+					</Placemark>}
+				</Map>
+      </YMaps>
+    </div>
+  );
+
+  function click(e) {
+    var coords = e.get('coords');
+    console.log(coords);
+
+    const name = prompt('Название метки')
+    const ballon = prompt('описание')
+    console.log(name, ballon)
+		setPlacemarks({'coords': coords, 'name': name, 'ballon': ballon})
+  }
 }
 
 export default Mapyandex;
