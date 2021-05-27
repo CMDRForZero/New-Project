a2a4a19a4ceb0e082e9ced448e04bcec7453626d
Vessels.TSS <an2288822@gmail.com>
2021-05-27 16:09:20 +0300

    - 1st phase REACT

---
+++ frontend/src/components/actions-header__user.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './search.css';
+import Usericon from 'user-header__icon';
+import Usermenu from 'user-header__menu';
+
+const Headeruser = () => {
+	return (
+		<div class="actions-header__user user-header">
+			<Usericon/>
+			<Usermenu/>
+		</div>
+	);
+}
+
+export default Headeruser;
+
+
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './css/search.css';
+import Header from './components/header';
+//import Maps from './components/map';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Wrapper = () => {
+	return (
+		<Header />
+	);
+}
+
+ReactDOM.render(<Wrapper/>, document.getElementById('Wrapper'));
