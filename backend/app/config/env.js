37a881cbbe84597d912d6478c4ba5891293e005c
Alex Feoktistov <finddelst@gmail.com>
2021-07-29 12:51:33 +0000

    Update env.js

--- backend/app/config/env.js
+++ backend/app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'clubmap',
     username: 'postgres',
     password: 'secret',
-    host: 'postgres',
+    host: 'localhost',
     dialect: 'postgres',
     pool: {
         max: 5,
